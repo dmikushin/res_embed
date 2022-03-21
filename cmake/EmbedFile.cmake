05984e720111d71f0dcc03982d658a4c39a350d5
Dmitry Mikushin <dmitry@kernelgen.org>
2022-03-21 23:26:48 +0100

    More corrections for Windows

--- cmake/EmbedFile.cmake
+++ cmake/EmbedFile.cmake
@@ -11,12 +11,6 @@ endif()
 
 get_filename_component(EMBED_FILE_EXT ${EMBED_FILE_PATH} LAST_EXT)
 
-if (MSVC OR APPLE)
-set(RES_EMBED_ASM_IN "${CMAKE_CURRENT_INCLUDE_DIR}/res_embed.nasm.in")
-else()
-set(RES_EMBED_ASM_IN "${CMAKE_CURRENT_INCLUDE_DIR}/res_embed.gas.in")
-endif()
-
 # Substitute encoded HEX content into template source file
 if ("${EMBED_FILE_EXT}" STREQUAL ".cpp")
 configure_file("${CMAKE_CURRENT_INCLUDE_DIR}/res_embed.cpp.in" ${EMBED_FILE_PATH})
@@ -24,6 +18,7 @@ elseif ("${EMBED_FILE_EXT}" STREQUAL ".asm")
 if (APPLE)
 set(OS_DEPENDENT_PREFIX "_")
 endif()
+message(STATUS "${RES_EMBED_ASM_IN}")
 configure_file("${RES_EMBED_ASM_IN}" ${EMBED_FILE_PATH})
 else()
 message(FATAL_ERROR "Unknown embedded file template extension: ${EMBED_FILE_EXT}")
