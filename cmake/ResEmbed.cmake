159cf77b40d226bf4639c44aabf1284db56254c9
Dmitry Mikushin <dmitry@kernelgen.org>
2023-05-24 07:42:35 -0400

    Getting res_embed to work correctly, when res_embed project is not a direct child of its client project. In order to do that, we convert macro to a function and use CMAKE_CURRENT_FUNCTION_LIST_DIR, which requires CMake 3.17

--- cmake/ResEmbed.cmake
+++ cmake/ResEmbed.cmake
@@ -1,47 +1,44 @@
 cmake_policy(SET CMP0057 NEW)
 
-if(NOT DEFINED RES_EMBED_CURRENT_INCLUDE_DIR)
-  set(RES_EMBED_CURRENT_INCLUDE_DIR ${CMAKE_CURRENT_LIST_DIR}/../include)
-endif()
-
-set(_RES_EMBED_PATH ${CMAKE_CURRENT_LIST_DIR})
-
-if(NOT DEFINED USE_NASM)
-if (MSVC OR APPLE)
-set(USE_NASM TRUE)
-else()
-set(USE_NASM FALSE)
-endif()
-endif()
-
-if (USE_NASM)
-find_package(Nasm REQUIRED)
-enable_language(ASM_NASM)
-if(NOT CMAKE_ASM_NASM_COMPILER_LOADED)
-message(FATAL_ERROR "NASM assembler not found, please install NASM")
-endif()
-set(RES_EMBED_ASM_IN "${RES_EMBED_CURRENT_INCLUDE_DIR}/res_embed.nasm.in")
-set(RES_EMBED_ASM_EXT ".nasm")
-else()
-enable_language(ASM)
-set(RES_EMBED_ASM_IN "${RES_EMBED_CURRENT_INCLUDE_DIR}/res_embed.gas.in")
-set(RES_EMBED_ASM_EXT ".s")
-endif()
-
-# Convert file into a binary blob and embed it into a C++ source file.
-macro(res_embed)
+# Convert file into a binary blob, embed it into an ASM file, and generate a C++ interface for it.
+function(res_embed)
 	set(options KEYWORD)
 	set(oneValueArgs TARGET NAME PATH)
 	set(multiValueArgs DEPENDS)
 	cmake_parse_arguments(RES_EMBED "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN} )
 	set(EMBED_FILE_CPP_PATH "${CMAKE_CURRENT_BINARY_DIR}/${RES_EMBED_NAME}.cpp")
 
+	set(_RES_EMBED_PATH ${CMAKE_CURRENT_FUNCTION_LIST_DIR})
+	set(RES_EMBED_CURRENT_INCLUDE_DIR ${_RES_EMBED_PATH}/../include)
+
+	if(NOT DEFINED USE_NASM)
+		if (MSVC OR APPLE)
+			set(USE_NASM TRUE)
+		else()
+			set(USE_NASM FALSE)
+		endif()
+	endif()
+
+	if (USE_NASM)
+		find_package(Nasm REQUIRED)
+		enable_language(ASM_NASM)
+		if(NOT CMAKE_ASM_NASM_COMPILER_LOADED)
+			message(FATAL_ERROR "NASM assembler not found, please install NASM")
+		endif()
+			set(RES_EMBED_ASM_IN "${RES_EMBED_CURRENT_INCLUDE_DIR}/res_embed.nasm.in")
+			set(RES_EMBED_ASM_EXT ".nasm")
+		else()
+		enable_language(ASM)
+		set(RES_EMBED_ASM_IN "${RES_EMBED_CURRENT_INCLUDE_DIR}/res_embed.gas.in")
+		set(RES_EMBED_ASM_EXT ".s")
+	endif()
+
 	add_custom_command(
 		OUTPUT ${EMBED_FILE_CPP_PATH}
 		COMMAND ${CMAKE_COMMAND} -DCMAKE_CURRENT_INCLUDE_DIR=${RES_EMBED_CURRENT_INCLUDE_DIR} -DFILE_KEY=${RES_EMBED_NAME} -DFILE_PATH=${RES_EMBED_PATH} -DEMBED_FILE_PATH=${EMBED_FILE_CPP_PATH} -P ${_RES_EMBED_PATH}/EmbedFile.cmake
 		COMMENT "Adding file ${RES_EMBED_PATH}"
 		DEPENDS "${RES_EMBED_CURRENT_INCLUDE_DIR}/res_embed.cpp.in" "${RES_EMBED_PATH}")
-	set_source_files_properties("${EMBED_FILE_CPP_PATH}" PROPERTIES GENERATED TRUE) 
+	set_source_files_properties("${EMBED_FILE_CPP_PATH}" PROPERTIES GENERATED TRUE)
 
 	target_sources(${RES_EMBED_TARGET} PRIVATE ${EMBED_FILE_CPP_PATH})
 
@@ -93,5 +90,5 @@ macro(res_embed)
 	endif()
 
 	message(STATUS "Resource file ${RES_EMBED_PATH} shall be added to target ${RES_EMBED_TARGET}")
-endmacro()
+endfunction()
 
